75ab976d5ff34ba30cab2e32ba64a6e2f44c83c7
allenlyu <allen.walker.lc@gmail.com>
2017-11-27 18:17:06 +0800

    server init

---
+++ socket/client.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+int main(int argc, char *argv[]){
+    int client_sockfd;
+    int len;
+    struct sockaddr_in my_addr;
+    int sin_size;
+    char buf[BUFSIZ];
+    memset(&my_addr,0,sizeof(my_addr));
+    my_addr.sin_family=AF_INET;
+    my_addr.sin_addr.s_addr=INADDR_ANY;
+    
+
+
+    return 0;
+}
