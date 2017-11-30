05ad57d897cbddee2fa6757bc3034a5dea8b9427
allenlyu <allen.walker.lc@gmail.com>
2017-11-30 19:20:05 +0800

    1

--- server.c
+++ server.c
@@ -1,17 +1,16 @@
 #include <pthread.h>
 #include "stdio.h"
 #include <unistd.h> 
-
 #define NUM 6
 int a = 0;
-pthread_mutex_t lock;
+spin_lock_t lock;
 int main()
 {
     void print_msg(void*);
     pthread_mutex_init(&lock,NULL);
     pthread_t t1,t2;
     pthread_create(&t1,NULL,(void*)print_msg,(void *)"t1\n");
-    pthread_create(&t2,NULL,(void*)print_msg,(void *)"t2\n");
+    pthread_create(&t2,NULL,(void*)print_msg,(void *)"t2\n");   
    
     pthread_join(t1,NULL);
     pthread_join(t2,NULL);  
@@ -21,9 +20,9 @@ void print_msg(void* m)
 {
     char *cp=(char*)m;
 	for(int i = 0;i<10000;i++){
-        pthread_mutex_lock(&lock);
+        // pthread_mutex_lock(&lock);
 		a++;
-        pthread_mutex_unlock(&lock);
+        // pthread_mutex_unlock(&lock);
 	}
     
 }
