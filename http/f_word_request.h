597a6c66a94eebb19732f4984c8f3bfc0d84d7b2
allenlyu <allen.walker.lc@gmail.com>
2017-12-11 17:17:31 +0800

    a proto

---
+++ http/f_word_request.h
@@ -0,0 +1,14 @@
+//
+// Created by baofeng on 17-12-11.
+//
+
+#ifndef F_WORD_SERVER_F_WORD_REQUEST_H
+#define F_WORD_SERVER_F_WORD_REQUEST_H
+
+
+
+#define F_WORD_HTML 1
+#define F_WORD_JS 2
+#define F_WORD_CSS 3
+
+#endif //F_WORD_SERVER_F_WORD_REQUEST_H
--- socket/server.c
+++ socket/server.c
@@ -2,14 +2,13 @@
 #include "string.h"
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <fcntl.h> // for open
 #include <unistd.h> // for close
 #define LISTEN 1
 int main(int argc,char **argvs){
     int listenfd,clientfd;
     socklen_t len;
     struct sockaddr_in serveraddr,clientaddr;
-    char buff[1024];
+    char buff[128];
 
     listenfd = socket(AF_INET,SOCK_STREAM,0);
 
@@ -23,34 +22,28 @@ int main(int argc,char **argvs){
     listen(listenfd,1024);
 
     memset(buff,0,sizeof(buff));
-    while(LISTEN){
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wmissing-noreturn"
+    while(1){
         len = sizeof(clientaddr);
         clientfd = accept(listenfd,(struct sockaddr *)&clientaddr,&len);
-        // printf("connect from %s,port %d \n",inet_ntop(AF_INET,&clientaddr,buff,sizeof(buff)),ntohs(clientaddr.sin_port));
-        // snprintf(buff,sizeof(buff),"HTTP/1.1 200 OK \r\nContent-type: text\\plain \r\n \r\n\r\n");
-        
-        // snprintf(buff,sizeof(buff),"\r\n\r\n");
-        // snprintf(buff,sizeof(buff),"qqq");
 
         FILE *f = fopen("/home/baofeng/Project/f-word-server/socket/http_example","r");
-
-        while(!feof(f)){
-            fscanf(f,"%s",buff);
-            printf("%s",buff);
-        }
+        fread(buff,sizeof(char),1024,f);
 
         fclose(f);
-    
+        printf("%s",buff);
 
         char recv_buff[1024];
         read(clientfd,&recv_buff,sizeof(recv_buff));
-        printf("%s",recv_buff);
+//        printf("%s",recv_buff);
 
 
         write(clientfd,buff,sizeof(buff));
         memset(buff,0,sizeof(buff));
         close(clientfd);
     }
+#pragma clang diagnostic pop
 
 
 }
